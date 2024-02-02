54f048e60090d1277a10cbf8635357a8d36ba32c
Jakub <jakub.szczepanski02@gmail.com>
2024-02-02 21:05:21 +0100

    zaktualizowano 'ideas'

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ menu.show_main_menu()
 '''
 [IDEAS]
  [] możliwośc zapauzowania i wznowienia gry
- [] wyświetlanie punktów w czasie rzeczywistym
  [] spowolnienie i zmiana prędkości wraz ze wzrostem punktów
  [] możliwość wyboru wielkości ekranu
  [] dodanie dodatkowej muzyki (po najechaniu na kafelek)
+ [] dodanie ramki do tekstu z punktami
 [ZREALIZOWANO]
+ [X] wyświetlanie punktów w czasie rzeczywistym
  [X] dodanie muzyki
  [X] ekran końcowy z ilością punktów
  [X] możliwośc gry drugiego gracza po śmierci pierwszego
